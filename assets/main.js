235b67d3e3e09897ded08e43b538e88bc9005e72
Johnny McGee <johnnymcgee.dev@gmail.com>
2025-07-31 23:04:53 -0500

    Condense site navigation and focus on style

--- assets/main.js
+++ assets/main.js
@@ -1,105 +1,88 @@
 (function () {
-    const menuButton = document.getElementById("menu-button");
-    menuButton?.addEventListener("click", handleClickMenu);
+  const menuButton = document.getElementById("menu-button");
+  menuButton?.addEventListener("click", handleClickMenu);
 
-    animateElementsInView();
-    document.addEventListener("scroll", throttle(animateElementsInView, 100));
-    document.addEventListener("scroll", headerScrollListener());
+  animateElementsInView();
+  document.addEventListener("scroll", throttle(animateElementsInView, 100));
+  document.addEventListener("scroll", hideDownIcon);
 
-    function throttle(fn, delay) {
-        let lastCall = 0;
-        return function (...args) {
-            const now = new Date().getTime();
-            if (now - lastCall < delay) return;
-            lastCall = now;
-            return fn(...args);
-        };
+  function hideDownIcon() {
+    if (window.scrollY >= window.innerHeight) {
+      const el = document.querySelector(".down-icon");
+      el?.remove();
+      document.removeEventListener("scroll", hideDownIcon);
+    }
+  }
+
+  function throttle(fn, delay) {
+    let lastCall = 0;
+    return function (...args) {
+      const now = new Date().getTime();
+      if (now - lastCall < delay) return;
+      lastCall = now;
+      return fn(...args);
+    };
+  }
+
+  function animateElementsInView() {
+    const animatedElements = document.querySelectorAll(".animate-in");
+    animatedElements.forEach((el) => {
+      const isInView =
+        el.parentElement?.getBoundingClientRect().top < window.innerHeight;
+      const isRunning = el.style["animation-play-state"] === "running";
+      if (!isInView || isRunning) return;
+
+      el.style["animation-delay"] = `${offsetMs(el)}ms`;
+      el.style["animation-play-state"] = "running";
+    });
+  }
+
+  function offsetMs(el, maxDelay = 500) {
+    const offsetFromTop = el.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+    return maxDelay * (offsetFromTop / windowHeight);
+  }
+
+  function handleClickMenu(event) {
+    const button = event.currentTarget;
+
+    if (!button) {
+      throw new Error("Menu button not found");
     }
 
-    function headerScrollListener() {
-        let referenceScroll = window.scrollY;
-        let lastScroll = window.scrollY;
+    const drawer = document.getElementById("menu-drawer");
 
-        return () => {
-            const header = document.querySelector("header");
-
-            const dy = window.scrollY - referenceScroll;
-
-            if (dy > 100) {
-                header.classList.add("header--scrolled");
-            }
-
-            if (dy < -20 || window.scrollY < 100) {
-                header.classList.remove("header--scrolled");
-            }
-
-            if ((window.scrollY < lastScroll && window.scrollY > referenceScroll) || (window.scrollY > lastScroll && window.scrollY < referenceScroll)) {
-                referenceScroll = window.scrollY;
-            }
-
-            lastScroll = window.scrollY;
-        };
+    if (!drawer) {
+      throw new Error("Menu drawer not found");
     }
 
-    function animateElementsInView() {
-        const animatedElements = document.querySelectorAll(".animate-in");
-        animatedElements.forEach((el) => {
-            const isInView = el.parentElement?.getBoundingClientRect().top < window.innerHeight;
-            const isRunning = el.style['animation-play-state'] === 'running';
-            if (!isInView || isRunning) return;
+    const isOpen = drawer.classList.contains("drawer--open");
 
-            el.style['animation-delay'] = `${offsetMs(el)}ms`;
-            el.style['animation-play-state'] = 'running';
-        });
+    function closeOnScroll() {
+      closeMenu(button, drawer);
+      document.removeEventListener("scroll", closeOnScroll);
     }
 
-    function offsetMs(el, maxDelay = 500) {
-        const offsetFromTop = el.getBoundingClientRect().top;
-        const windowHeight = window.innerHeight;
-        return maxDelay * (offsetFromTop / windowHeight);
+    if (isOpen) {
+      closeMenu(button, drawer);
+      document.removeEventListener("scroll", closeOnScroll);
+
+      return;
     }
 
-    function handleClickMenu(event) {
-        const button = event.currentTarget;
+    openMenu(button, drawer);
+    document.addEventListener("scroll", closeOnScroll);
+  }
 
-        if (!button) {
-            throw new Error("Menu button not found");
-        }
+  function closeMenu(button, drawer) {
+    button.classList.remove("hamburger--open");
+    drawer.classList.remove("drawer--open");
+    drawer.setAttribute("aria-hidden", "true");
+  }
 
-        const drawer = document.getElementById("menu-drawer");
-
-        if (!drawer) {
-            throw new Error("Menu drawer not found");
-        }
-
-        const isOpen = drawer.classList.contains("drawer--open");
-
-        function closeOnScroll() {
-            closeMenu(button, drawer);
-            document.removeEventListener("scroll", closeOnScroll);
-        }
-
-        if (isOpen) {
-            closeMenu(button, drawer);
-            document.removeEventListener("scroll", closeOnScroll);
-
-            return;
-        }
-
-        openMenu(button, drawer);
-        document.addEventListener("scroll", closeOnScroll);
-    }
-
-    function closeMenu(button, drawer) {
-        button.classList.remove("hamburger--open");
-        drawer.classList.remove("drawer--open");
-        drawer.setAttribute("aria-hidden", "true");
-    }
-
-    function openMenu(button, drawer) {
-        button.classList.add("hamburger--open");
-        drawer.classList.add("drawer--open");
-        drawer.removeAttribute("aria-hidden");
-    }
-}
-)();
+  function openMenu(button, drawer) {
+    button.classList.add("hamburger--open");
+    drawer.classList.add("drawer--open");
+    drawer.removeAttribute("aria-hidden");
+  }
+})();
