00c56fc1535dff4de842fc5569ab5996560f076f
Johnny McGee <johnnymcgee.dev@gmail.com>
2025-08-01 05:01:15 -0500

    Add secret message

---
+++ assets/secret-message-🤫.js
@@ -0,0 +1,15 @@
+(function () {
+  const message = `
+🤠 Howdy ho, neighbor!
+
+Thanks for visiting johnnymcgee.dev.
+
+If you like, you can connect with me on LinkedIn:
+
+https://www.linkedin.com/in/johnny-mcgee-19a181129.
+
+Have a great day!
+`;
+
+  console.log(message);
+})();
