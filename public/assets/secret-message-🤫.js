44dc421d86357625ec611276704ddb99be2560c5
Johnny McGee <johnnymcgee.dev@gmail.com>
2025-08-01 23:13:32 -0500

    🖖 Nanu Nanu

--- public/assets/secret-message-🤫.js
+++ public/assets/secret-message-🤫.js
@@ -1,14 +1,18 @@
 (function () {
   const message = `
-🤠 Howdy ho, neighbor!
+🤠 Howdy ho, neighbor! 🏜️
 
-Thanks for visiting johnnymcgee.dev.
+Thanks for dropping in.
 
-If you like, you can connect with me on LinkedIn:
+If you like, you can connect with me
 
+on LinkedIn:
 https://www.linkedin.com/in/johnny-mcgee-19a181129.
 
-Have a great day!
+or GitHub:
+https://github.com/JohnnyMcGee
+
+🖖 Nanu Nanu! 🚀
 `;
 
   console.log(message);
